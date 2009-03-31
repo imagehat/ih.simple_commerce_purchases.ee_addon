6418901a426899e8a3fa27e5d16d079cdf2e16e0
Mike Kroll <imagehat@gmail.com>
2009-03-31 15:44:28 -0500

    Updated usage

--- pi.ih_simple_commerce_purchases.php
+++ pi.ih_simple_commerce_purchases.php
@@ -134,21 +134,23 @@ class Ih_simple_commerce_purchases
 Allows you to list information for purchases made through the
 Simple Commerce Module for any individual member.
 
-
+---------------------------
 Tag Example:
-------------
+---------------------------
 	
 {exp:ih_simple_commerce_purchases member_id="{segment_3}"}
 	<p>{item_purchased} - {purchase_date format="%F %d, %Y"}</p>
 {exp:ih_simple_commerce_purchases}
 
+---------------------------
 Available parameters:
----------------------
+---------------------------
 
 member_id: Member ID to display purchases for. If no member id is set will default to current logged in member.
 orderby: 'item' or 'date'. Defaults to 'date'.
 sort: 'asc' or 'desc'. Defaults to 'desc'.
 
+---------------------------
 Available variables:
 ---------------------------
 
@@ -164,6 +166,12 @@ Available variables:
 
 {count}
 {total_results}
+
+---------------------------
+Change Log
+---------------------------
+
+1.0.0 - Initial Release
 
 
 <?php
